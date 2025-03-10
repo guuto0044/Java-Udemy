87552517b60547eb13daf126b1905c4a8af0d2b4
Guto <guuto0044@gmail.com>
2025-03-10 15:38:01 -0300

    Att.2

---
+++ src/com/cursojava/secao10/Foreach.java
@@ -0,0 +1,11 @@
+package com.cursojava.secao10;
+
+public class Foreach {
+    public static void main(String[] args) {
+        String [] vect = new String[]{"Guto","Augusto","Lais"};
+
+        for (String names : vect){
+            System.out.println(names);
+        }
+    }
+}
