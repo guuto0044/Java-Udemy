c53ab76cc40848421841fc92903fbb5708f36da1
Guto <guuto0044@gmail.com>
2025-03-10 15:18:00 -0300

    Att

--- src/com/cursojava/secao10/Boxing_Unboxing_WrapperClass.java
+++ src/com/cursojava/secao10/Boxing_Unboxing_WrapperClass.java
@@ -1,7 +1,18 @@
 package com.cursojava.secao10;
 
+import java.util.Objects;
+
 public class Boxing_Unboxing_WrapperClass {
-//   Boxing : É o processo de conversão de um objeto tipo valor para um objeto tipo refereência compatível.
+//   Boxing (Encaixotamento) : É o processo de conversão de um objeto tipo valor para um objeto tipo refereência compatível.
+
+//   Unboxing:
+//    public static void main(String[] args) {
+//        //   Boxing
+//        int x = 20;
+//        Object obj = x;
+//        System.out.println(obj);
+//    }
+
     public static void main(String[] args) {
 
     }
