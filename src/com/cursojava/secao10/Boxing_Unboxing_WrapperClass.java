3c0b5ebfdf9041e1fa0fb69d5ac0a1fce9e953a5
Guto <guuto0044@gmail.com>
2025-03-10 15:25:05 -0300

    Att

--- src/com/cursojava/secao10/Boxing_Unboxing_WrapperClass.java
+++ src/com/cursojava/secao10/Boxing_Unboxing_WrapperClass.java
@@ -3,17 +3,30 @@ package com.cursojava.secao10;
 import java.util.Objects;
 
 public class Boxing_Unboxing_WrapperClass {
-//   Boxing (Encaixotamento) : É o processo de conversão de um objeto tipo valor para um objeto tipo refereência compatível.
+//   Boxing (Encaixotamento) : É o processo de conversão de um objeto tipo valor para um objeto tipo refereência
+//   compatível.
+//   Unboxing: É o processo de conversão de um objeto tipo refeência para um objeto tipo valor compatível.
+//   Wrapper Class: São classes do tipo primitivo. Boxing e Unboxing são naturais da Classe.
+//    Seu uso é comum em entidades de sistemas de informação. Pois tipos referencia(Classes) aceitam valor null e
+//    usufruem do reucursos OO.
 
 //   Unboxing:
 //    public static void main(String[] args) {
-//        //   Boxing
+//        Boxing:
 //        int x = 20;
 //        Object obj = x;
 //        System.out.println(obj);
 //    }
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//        Unboxing
+//        int x = 20;
+//        Object obj = x;
+//        System.out.println(obj);
+//        int y = (int) obj;
+//        System.out.println(y);
+//    }
 
-    }
+
+
 }
