13c0a8533f70c94984b4443af5cad8fcebc6c82e
Guto <guuto0044@gmail.com>
2025-03-12 11:28:52 -0300

    Att.03-12

--- src/com/cursojava/secao10/ProgramList.java
+++ src/com/cursojava/secao10/ProgramList.java
@@ -2,7 +2,7 @@ package com.cursojava.secao10;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class ProgramList {
     public static void main(String[] args) {
